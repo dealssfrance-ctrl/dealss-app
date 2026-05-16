9f088c746209bdf4947c3969505a31949da3c350
dealssfrance-ctrl <dealssfrance-ctrl@users.noreply.github.com>
2026-05-16 22:31:11 +0000

    deploy: 90d75661428529f1829508ae331629f145ee8644

---
+++ assets/index-BU-5i27O.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-BRMPn7GD.js","assets/index-BY2riKwe.js","assets/index-Dwt10oxW.css"])))=>i.map(i=>d[i]);
+import{r as i,_ as r}from"./index-BY2riKwe.js";const _=i("Device",{web:()=>r(()=>import("./web-BRMPn7GD.js"),__vite__mapDeps([0,1,2])).then(e=>new e.DeviceWeb)});export{_ as Device};
